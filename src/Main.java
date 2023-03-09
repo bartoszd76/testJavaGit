4bf39b6cf55ad7cab7bda50ab44430fb158a3c2e
Bartosz Dudek <bartoszd@poczta.onet.pl>
2023-03-09 22:01:56 +0100

    Aktualizacja 2x jakoś idzie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj Świecie!");
         System.out.println("Jakoś idzie :)");
+        System.out.println("Jakoś idzie :)");
     }
 }
